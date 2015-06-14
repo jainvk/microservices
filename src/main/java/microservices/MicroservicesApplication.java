17e68670a2b04e00ce733c1ed2b86c937d43c3d6
vk jain <vkjain@hotmail.com>
2015-06-13 22:45:31 -0500

    Autoconfiguration example

--- src/main/java/microservices/MicroservicesApplication.java
+++ src/main/java/microservices/MicroservicesApplication.java
@@ -1,9 +1,12 @@
 package microservices;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+//@ComponentScan
 public class MicroservicesApplication {
 
     public static void main(String[] args) {
